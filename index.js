1378dc47b3f6a3d61d631e253f66ac20140e9646
RoseMuster <rosenjdk@gmail.com>
2026-03-19 21:09:38 -0400

    Update website

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('wekjfbjwebf')
+function openMenu(){
+   document.body.body.classlist += " menu--open" 
+}
+
+function closedMenu(){
+   document.body.classlist.remove( 'menu--close') 
+}
